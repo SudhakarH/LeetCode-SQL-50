f493d07293dd346482c48f3f033202653b206b27
Sudhakar Hanumanth <44610380+SudhakarH@users.noreply.github.com>
2024-06-23 22:08:22 +0200

    Create Department Top Three Salaries.sql

---
+++ Department Top Three Salaries.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+with temp as 
+(
+    select d.name as Department, e.name as Employee, e.salary as Salary, dense_rank() over (partition by d.name order by e.salary desc) as ranking
+    from employee e
+    left join department d
+    on e.departmentid = d.id
+)
+select Department, Employee, Salary from temp where ranking <4;
